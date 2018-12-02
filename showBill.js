b74be1ded8041176383475c283fbbe24c35383f1
Kenju Wagatsuma <ken901waga@gmail.com>
2018-12-02 14:39:56 +0900

    decompose the statement function to amountFor function

--- showBill.js
+++ showBill.js
@@ -2,17 +2,7 @@ const fs = require('fs');
 const util = require('util');
 
 function statement(invoice, plays) {
-  let totalAmount = 0;
-  let volumeCredits = 0;
-  let result = `Statement for ${invoice.customer}\n`;
-  const format = new Intl.NumberFormat("en-US",
-    {
-      style: "currency", currency: "USD",
-      minimumFractionDigits: 2
-    }).format;
-
-  for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
+  function amountFor(perf, play) {
     let thisAmount = 0;
 
     switch (play.type) {
@@ -32,6 +22,22 @@ function statement(invoice, plays) {
       default:
         throw new Error(`unknown type: ${play.type}`);
     }
+
+    return thisAmount;
+  }
+
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `Statement for ${invoice.customer}\n`;
+  const format = new Intl.NumberFormat("en-US",
+    {
+      style: "currency", currency: "USD",
+      minimumFractionDigits: 2
+    }).format;
+
+  for (let perf of invoice.performances) {
+    const play = plays[perf.playID];
+    let thisAmount = amountFor(perf, play);
 
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
